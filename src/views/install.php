a4e8d3369b82f850c74d8e9be793617eb0527360
tien.xuan.vo <tien.xuan.vo@gmail.com>
2017-10-03 12:54:01 +0700

    Install database

--- src/views/install.php
+++ src/views/install.php
@@ -1,5 +1,7 @@
 <?php
 
+echo 'Reinstalling...<br>';
+
 $host       = 'mysql';
 $username   = 'root';
 $password   = 'root';
@@ -8,74 +10,84 @@ $dbname     = 'db';
 $db = new mysqli($host, $username, $password);
 // Check connection
 if ($db->connect_error) {
-    die('Connection failed: ' . $db->connect_error);
-}
-
-// Create database.
-if (!$db->select_db($dbname)) {
-    die("Error selecting database': {$db->error}\n");
+    die("Connection failed: {$db->connect_error}\n");
 }
 
 if ($db->select_db($dbname)) {
-    // Create user table.
-    $sql = "CREATE TABLE user (
-        id int(11) NOT NULL AUTO_INCREMENT,
-        username varchar(255) NOT NULL,
-        password varchar(255) NOT NULL,
-        role varchar(255) NOT NULL,
-        PRIMARY KEY (id),
-        UNIQUE KEY username (username)
-    )";
-
-    if ($db->query($sql) === TRUE) {
-        echo 'Table user created successfully';
-    } else {
-        echo 'Error creating table: ' . $db->error;
+    // Reinstall.
+    if (isset($_GET['reinstall']) && $_GET['reinstall'] === 'yes') {
+        $db->query('DROP TABLE IF EXISTS user');
+        $db->query('DROP TABLE IF EXISTS item');
     }
 
-    // Create item table.
-    $sql = "CREATE TABLE item (
-        id int(11) NOT NULL AUTO_INCREMENT,
-        name varchar(255) NOT NULL,
-        PRIMARY KEY (id)
-    )";
+    if(!$db->query('DESCRIBE user')) {
+        // Create user table.
+        $sql = "CREATE TABLE user (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            username varchar(255) NOT NULL,
+            password varchar(255) NOT NULL,
+            role varchar(255) NOT NULL,
+            PRIMARY KEY (id),
+            UNIQUE KEY username (username)
+        )";
 
-    if ($db->query($sql) === TRUE) {
-        echo 'Table item created successfully';
-    } else {
-        echo 'Error creating table: ' . $db->error;
+        if ($db->query($sql) === TRUE) {
+            echo 'Table user created successfully<br>';
+        } else {
+            echo "Error creating table: {$db->error}<br>";
+        }
+
+        // Create users.
+        $sql = "INSERT INTO user (username, password, role) VALUES
+            ('admin', '" . password_hash('admin', PASSWORD_DEFAULT) . "', 'admin'),
+            ('user1', '" . password_hash('user1', PASSWORD_DEFAULT) . "', 'user'),
+            ('user2', '" . password_hash('user2', PASSWORD_DEFAULT) . "', 'user'),
+            ('user3', '" . password_hash('user3', PASSWORD_DEFAULT) . "', 'user'),
+            ('guest1', '" . password_hash('guest1', PASSWORD_DEFAULT) . "', 'guest'),
+            ('guest2', '" . password_hash('guest2', PASSWORD_DEFAULT) . "', 'guest')";
+
+        if ($db->query($sql) === TRUE) {
+            echo 'New users created successfully<br>';
+        } else {
+            echo "Error creating users: {$db->error}<br>";
+        }
     }
 
-    // Create users.
-    $sql = "INSERT INTO user (username, password, role)
-        VALUES ('admin', '" . password_hash('admin', PASSWORD_DEFAULT) . "', 'admin'),
-        VALUES ('user1', '" . password_hash('user1', PASSWORD_DEFAULT) . "', 'user'),
-        VALUES ('user2', '" . password_hash('user2', PASSWORD_DEFAULT) . "', 'user'),
-        VALUES ('user3', '" . password_hash('user3', PASSWORD_DEFAULT) . "', 'user'),
-        VALUES ('guest1', '" . password_hash('guest1', PASSWORD_DEFAULT) . "', 'guest'),
-        VALUES ('guest2', '" . password_hash('guest2', PASSWORD_DEFAULT) . "', 'guest')";
+    if(!$db->query('DESCRIBE item')) {
+        // Create item table.
+        $sql = "CREATE TABLE item (
+            id int(11) NOT NULL AUTO_INCREMENT,
+            name varchar(255) NOT NULL,
+            PRIMARY KEY (id)
+        )";
 
-    if ($db->query($sql) === TRUE) {
-        echo 'New users created successfully';
-    } else {
-        echo 'Error creating users: ' . $db->error;
-    }
+        if ($db->query($sql) === TRUE) {
+            echo 'Table item created successfully<br>';
+        } else {
+            echo "Error creating table: {$db->error}<br>";
+        }
 
-    // Create items.
-    $sql = "INSERT INTO item (name)
-        VALUES ('Book'),
-        VALUES ('T-Shirt'),
-        VALUES ('Chair'),
-        VALUES ('Table'),
-        VALUES ('Laptop'),
-        VALUES ('Bottle'),
-        VALUES ('I-Phone'),
-        VALUES ('Refrigerator')";
+        // Create items.
+        $sql = "INSERT INTO item (name) VALUES
+            ('Book'),
+            ('T-Shirt'),
+            ('Chair'),
+            ('Table'),
+            ('Laptop'),
+            ('Bottle'),
+            ('I-Phone'),
+            ('Refrigerator')";
 
-    if ($db->query($sql) === TRUE) {
-        echo 'New items created successfully';
-    } else {
-        echo 'Error creating items: ' . $db->error;
+        if ($db->query($sql) === TRUE) {
+            echo 'New items created successfully<br>';
+        } else {
+            echo "Error creating items: {$db->error}<br>";
+        }
     }
 }
+else {
+    die("Error selecting database': {$db->error}<br>");
+}
 $db->close();
+
+echo 'Completed!<br>';
